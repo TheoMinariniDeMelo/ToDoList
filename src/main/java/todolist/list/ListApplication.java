b36e4f2f0ec27bcd9eca31694180e210b017bed4
TheoMinariniDeMelo <theominarini@outlook.com>
2023-08-10 16:44:18 -0300

    Create Models

--- src/main/java/todolist/list/ListApplication.java
+++ src/main/java/todolist/list/ListApplication.java
@@ -3,11 +3,13 @@ package todolist.list;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.*;
+
 @SpringBootApplication
 public class ListApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ListApplication.class, args);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(ListApplication.class, args);
-	}
 
 }
